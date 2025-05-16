d90a7294b5f133791151f85fd1ab5129320f416b
“Ivona” <ivona.rieger@gmail.com>
2025-05-16 21:31:53 +0200

    add navbarstyle

--- momentum/src/components/NavBar.js
+++ momentum/src/components/NavBar.js
@@ -4,17 +4,28 @@ import {
   Nav,
   Container,
  } from "react-bootstrap";
+import logo from "../assets/logo.png";
 
 const NavBar = () => {
     return (
     <Navbar expand="md" fixed="top">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand> <img src={logo} alt="logo" height="35" /> </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+          <Nav className="ml-auto">
+            <Nav.Link>
+                <i class="bi bi-house"></i>{' '}
+                Home
+                </Nav.Link>
+            <Nav.Link>
+                <i class="bi bi-box-arrow-in-right"></i>{' '}
+                Sign in
+                </Nav.Link>
+            <Nav.Link>
+                <i class="bi bi-door-open"></i>{' '}
+                Sign up
+                </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
