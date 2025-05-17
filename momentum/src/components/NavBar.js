6de4dea5b5cd2478fcdbcb29ecdb0da3902f419c
“Ivona” <ivona.rieger@gmail.com>
2025-05-17 21:20:12 +0200

    add navbat style

--- momentum/src/components/NavBar.js
+++ momentum/src/components/NavBar.js
@@ -5,10 +5,12 @@ import {
   Container,
  } from "react-bootstrap";
 import logo from "../assets/logo.png";
+import styles from "../styles/NavBar.module.css";
+
 
 const NavBar = () => {
     return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container fluid className="px-0">
         <Navbar.Brand className="ps-3"> <img src={logo} alt="logo" height="35" /> </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
