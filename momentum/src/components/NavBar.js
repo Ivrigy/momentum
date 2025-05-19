8fff4314b9aed5aa3e8e6e65c1e8e8bc5ce4f512
“Ivona” <ivona.rieger@gmail.com>
2025-05-19 15:06:17 +0200

    add routing

--- momentum/src/components/NavBar.js
+++ momentum/src/components/NavBar.js
@@ -6,28 +6,34 @@ import {
  } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 
 const NavBar = () => {
     return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container fluid className="px-0">
-        <Navbar.Brand className="ps-3"> <img src={logo} alt="logo" height="35" /> </Navbar.Brand>
+        <NavLink to="/">
+        <Navbar.Brand className="ps-3"> <img src={logo} alt="logo" height="35" /> </Navbar.Brand> </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link>
+            <NavLink exact
+              activeClassName={styles.Active}
+              to="/">
                 <i class="bi bi-house"></i>{' '}
                 Home
-                </Nav.Link>
-            <Nav.Link>
+                </NavLink>
+            <NavLink activeClassName={styles.Active}
+              to="/signin">
                 <i class="bi bi-box-arrow-in-right"></i>{' '}
                 Sign in
-                </Nav.Link>
-            <Nav.Link>
+                </NavLink>
+            <NavLink activeClassName={styles.Active}
+              to="/signup">
                 <i class="bi bi-door-open"></i>{' '}
                 Sign up
-                </Nav.Link>
+                </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
